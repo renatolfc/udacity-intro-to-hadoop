bb2e3460d93ea9f7e29f67d61340895a214de44b
Renato L. F. Cunha <renato@renatocunha.com>
2014-02-16 12:43:57 -0300

    popular-tags: stopped using the topN pattern, as it does not make sense

--- popular_tags_mapper.py
+++ popular_tags_mapper.py
@@ -8,20 +8,6 @@ import csv
 
 from common import getField, isValidNodeLine
 
-TOP_N_TAGS = 10
-
-def comparator(a, b):
-    "Custom comparator for sorting our list of tags."
-
-    # Equal keys have equal weights
-    if a[1] == b[1]:
-        return 0
-    # Keys that appear more should come first
-    elif a[1] < b[1]:
-        return 1
-    else:
-        return -1
-
 def mapper():
     """Mapper function.
 
@@ -53,14 +39,66 @@ def mapper():
         for tag in tags:
             tagDict[tag] = tagDict.get(tag, 0) + 1
 
-    # All tags, sorted by most frequent to least frequent
-    mostFrequent = sorted(list(tagDict.items()), cmp=comparator)
+    # NOTE: Funny thing happening here. We *cannot use* the top-N pattern
+    # explained in class! Suppose the following example: we want the top 1
+    # tag and we're using the top-N pattern presented in class. Now, suppose
+    # we have two mappers and we actually have only two tags: tag1, and tag2.
+    # Suppose our mappers see the following
+    # Mapper 1:
+    #   This gets all instances of tag1, which happen to be in this example,
+    #   equal to 1000. BUT it also sees 400 instances of tag two.
+    #   So, internally, we can have in mapper 1:
+    #   tag1 = 1000
+    #   tag2 = 400
+    #
+    # Mapper 2: Suppose mapper 2 sees only tag2 and its count is 900.
+    # So, the internal list for mapper 2 will be:
+    #   tag2 = 900
+    #
+    # Now, if we used the topN patter *as presented* in class, we'd be lost.
+    # Our reducer would see as its input:
+    #   tag1 = 1000
+    #   tag2 = 900
+    #
+    # And it would output the incorrect top1 tag, tag1, with 1000 occurrences.
+    #
+    # If, instead, the mappers output all tags they saw, then the reducer would
+    # have the chance to see this list:
+    #   tag1 = 1000
+    #   tag2 = 400
+    #   tag2 = 900
+    # Now it would be able to reduce correctly and output the top 1 tag, tag2.
 
-    # We only want to output the 10 most frequent from *this* split.
-    # Once the reducer(s) has(ve) the top 10 from all splits, it(they) will be
-    # able to get the global top 10
-    for tag in mostFrequent[:TOP_N_TAGS]:
+    # We print everything we got.
+    for tag in tagDict.items():
         print('%s\t%s' % tag)
+
+    # Addendum:
+    # Had I used the top N pattern, I would have defined a top-level function
+    # called comparator like this:
+    #
+    # def comparator(a, b):
+    #     "Custom comparator for sorting our list of tags."
+    #
+    #     # Equal keys have equal weights
+    #     if a[1] == b[1]:
+    #         return 0
+    #     # Keys that appear more should come first
+    #     elif a[1] < b[1]:
+    #         return 1
+    #     else:
+    #         return -1
+    #
+    # Then, instead of printing all the tags, I would have defined
+    # a mostFrequent list like this:
+    #
+    #  mostFrequent = sorted(list(tagDict.items()), cmp=comparator)
+    #
+    # Last, but not least, I would have printed the output like this:
+    #
+    # for tag in mostFrequent[:TOP_N_TAGS]:
+    #   print('%s\t%s' % tag)
+
 
 if __name__ == '__main__':
     mapper()
